5e89bf3daf9952966b4b20a08b06e97a56f9e98d
Felipe Henrique <solelipecosta1@gmail.com>
2020-09-14 18:44:53 -0400

    chore: subindo codigo

---
+++ Pixel Game/Assets/Scripts/FireButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FireButton : MonoBehaviour {
+	public Button button{get; private set;}
+	public static FireButton Instance;
+	
+	private void Awake(){
+		if(Instance == null) Instance = this;
+		else Destroy (gameObject);
+
+		button = GetComponent<Button>();
+	}
+}
---
+++ Pixel Game/Assets/Scripts/GameController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameController : MonoBehaviour
+{   
+    public int lastScore;
+    public int totalScore;
+    public int sceneLoadedInt;
+    public Text scoreText;
+
+    public GameObject gameOver;
+
+    public static GameController instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Pegar cena atual
+        Scene sceneLoaded = SceneManager.GetActiveScene();
+        sceneLoadedInt = sceneLoaded.buildIndex;
+        //Se for primeira cena, deletar score salvo 
+        if(sceneLoadedInt == 0){
+            PlayerPrefs.DeleteKey("LastScore");
+            if(PlayerPrefs.HasKey("LastScore")){
+                lastScore = PlayerPrefs.GetInt("LastScore");
+            }
+        //Senao tranferir score salvo pro score atual    
+        }else{
+            lastScore = PlayerPrefs.GetInt("LastScore");
+            totalScore = lastScore;
+            UpdateScoreText();
+        }
+
+
+        /* 
+        */
+        instance = this;
+    }
+    
+    //Salvar score atual e mostrar na tela
+    public void UpdateScoreText()
+    {
+        PlayerPrefs.SetInt("LastScore", totalScore);
+        Debug.Log( PlayerPrefs.GetInt("LastScore"));
+        scoreText.text = totalScore.ToString();
+    }
+
+    public void ShowGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+
+    public void RestartGame(string lvlName)
+    {
+        SceneManager.LoadScene(lvlName);
+    }
+}
